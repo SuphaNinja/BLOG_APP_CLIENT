7c00594787c4a040dbea5cd0cd9ecbad9a9c638f
Sid Björk <sidricobjork@gmail.com>
2024-07-06 15:50:25 +0200

    add

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5050', 
+    baseURL: 'https://blog-app-server-sable.vercel.app', 
     headers: {
         'x-access-token': localStorage.getItem("token"),
     },
